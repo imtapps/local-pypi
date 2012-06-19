f3fb53099f4a048877b821dcd60539e9cda658a7
root <root@asdf.(none)>
2012-06-19 18:13:06 +0000

    updated run

--- run.sh
+++ run.sh
@@ -1,1 +1,28 @@
-pypi-server --passwords=.password eggs &
+# symlink in /etc/init.d/localpypi
+# update-rc.d localpypi defaults <- to run on startup
+
+PIDFILE=localpypi.pid
+
+case "$1" in
+	start)
+		cmd="start-stop-daemon --start --make-pidfile --pidfile $PIDFILE --exec /root/local-pypi/pypi/bin/pypi-server -- --passwords=.password eggs"	
+		if $cmd; then
+			exit 0
+		else
+			exit 1
+		fi
+	;;
+	stop)
+		cmd="start-stop-daemon --stop --pidfile $PIDFILE"
+		if $cmd; then
+			exit 0
+		else
+			exit 1
+		fi
+	;;
+	*)
+		echo "Usage: /etc/init.d/localpypi {start|stop}"
+		exit 1
+esac
+
+exit 0
