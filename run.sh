c3ea2bb41ac4836cafa314d81c8ee417d03e3edb
Matthew J Morrison <matt.morrison@imtapps.com>
2012-11-29 13:18:14 -0600

    added tasktest branch

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn 'pypiserver:app("/home/appsdev/Dropbox/eggs",None,None,"/opt/localpypi/.pypipass")' --workers=1 --daemon --bind 127.0.0.1:8888
+/opt/local-pypi/virtualenv/bin/gunicorn 'pypiserver:app("/root/jenkins/Dropbox/eggs",None,None,None)' --workers=1 --daemon --bind 127.0.0.1:8888
